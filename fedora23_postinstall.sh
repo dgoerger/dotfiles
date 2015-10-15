e02869e452f3e56f0e4a3b2df5d06ebe8da1e270
David Goerger <dgoerger@users.noreply.github.com>
2015-10-14 23:34:05 -0400

    elinks

--- fedora23_postinstall.sh
+++ fedora23_postinstall.sh
@@ -26,7 +26,7 @@ sudo dnf install -y mesa-vdpau-drivers libva-vdpau-driver
 ########################
 ### commandline apps ###
 # all-around
-sudo dnf install -y git git-cal git-extras sl tig tmux traceroute vim-enhanced vim-fugitive
+sudo dnf install -y elinks git git-cal git-extras sl tig tmux traceroute vim-enhanced vim-fugitive
 # monitoring
 sudo dnf install -y bmon htop iotop iptraf-ng lsof ncdu
 # productivity
@@ -35,6 +35,7 @@ sudo dnf install -y pandoc-static transmission-cli
 sudo dnf install -y firewalld nmap ykpers
 sudo systemctl enable firewalld
 sudo firewall-cmd --set-default-zone=drop
+
 ### system libraries ###
 # fonts
 #-
@@ -45,14 +46,7 @@ sudo dnf install -y gstreamer1-plugins-bad-free gstreamer1-vaapi
 sudo dnf install -y texlive-collection-xetex
 # spellcheck
 sudo dnf install -y hunspell-en hunspell-es hunspell-de hunspell-fr
-## TODO: is the below still necessary for the Firefox dictionaries bug?
-## why is Canadian English not packaged separately?
-#UNUSED_HUNSPELL="de_AT.aff de_LI.aff en_BS.aff en_DK.aff en_IE.aff en_NA.aff en_SG.aff en_ZM.aff es_BO.dic es_CU.dic es_GT.aff es_PA.aff es_SV.aff fr_BE.aff fr_LU.aff de_AT.dic de_LI.dic en_BS.dic en_DK.dic en_IE.dic en_NA.dic en_SG.dic en_ZM.dic es_CL.aff es_GT.dic es_PA.dic es_SV.dic fr_BE.dic fr_LU.dic de_BE.aff de_LU.aff en_BW.aff en_GB.aff en_IN.aff en_NG.aff en_TT.aff en_ZW.aff es_CL.dic es_DO.aff es_HN.aff es_PE.aff es_US.aff fr_CA.aff fr_MC.aff de_BE.dic de_LU.dic en_BW.dic en_GB.dic en_IN.dic en_NG.dic en_TT.dic en_ZW.dic es_CO.aff es_DO.dic es_HN.dic es_PE.dic es_US.dic fr_CA.dic fr_MC.dic de_CH.aff en_AG.aff en_BZ.aff en_GH.aff en_JM.aff en_NZ.aff es_CO.dic es_EC.aff es_MX.aff es_PR.aff es_UY.aff fr_CH.aff  de_CH.dic en_AG.dic en_BZ.dic en_GH.dic en_JM.dic en_NZ.dic es_AR.aff es_CR.aff es_EC.dic es_MX.dic es_PR.dic es_UY.dic fr_CH.dic en_AU.aff en_HK.aff en_MW.aff en_PH.aff en_ZA.aff es_AR.dic es_CR.dic es_ES.aff es_NI.aff es_PY.aff es_VE.aff en_AU.dic en_HK.dic en_MW.dic en_PH.dic en_ZA.dic es_BO.aff es_CU.aff es_ES.dic es_NI.dic es_PY.dic es_VE.dic"
-#for i in ${UNUSED_HUNSPELL}; do
-#  if [ -f "/usr/share/myspell/${i}" ]; then
-#    sudo rm "/usr/share/myspell/${i}"
-#  fi
-#done
+
 ### graphical applications ###
 # multimedia
 sudo dnf install -y shotwell
@@ -60,6 +54,7 @@ sudo dnf install -y shotwell
 sudo chmod 444 /usr/libexec/shotwell/shotwell-video-thumbnailer
 # productivity
 sudo dnf install -y gnumeric keepassx
+
 ### GNOME tweaks ###
 # GNOME Shell
 sudo dnf install -y gnome-shell-extension-alternate-tab
@@ -86,10 +81,6 @@ dconf write /org/yorba/shotwell/preferences/files/commit-metadata true
 dconf write /org/yorba/shotwell/preferences/files/use-lowercase-filenames true
 dconf write /org/yorba/shotwell/preferences/ui/use-24-hour-time true
 dconf write /org/yorba/shotwell/preferences/ui/hide-photos-already-imported true
-## Locale
-#localectl set-locale LANG=en_CA.UTF-8
-#localectl set-x11-keymap us-mac mac
-#dconf write /system/locale/region "'en_CA.UTF-8'"
-#dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us+mac')]"
+
 ### Firefox ###
 sudo mkdir -p /usr/lib64/firefox/browser/defaults/preferences
