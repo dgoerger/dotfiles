d19c7f8b57497a4e94af43948b43ff3e2240ad3d
David Goerger <dgoerger@users.noreply.github.com>
2016-11-29 08:58:32 -0500

    thaaaat seems to have broken the default attribute

--- cookbooks/workstation/attributes/corporate.rb
+++ cookbooks/workstation/attributes/corporate.rb
@@ -1,6 +1,6 @@
 # intranets tend to have at least a few old things
 override['workstation']['crypto-policy'] = 'DEFAULT'
 # dnscrypt - not for corporate networks, probably
-override['workstation']['dnscrypt_providers'] = []
+override['workstation']['dnscrypt_providers'] = {}
 # don't try to override corporate dns policy, default instead from DHCP
-override['workstation']['dns_providers'] = []
+override['workstation']['dns_providers'] = {}
