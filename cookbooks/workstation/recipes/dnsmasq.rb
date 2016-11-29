05a994e178def45b796a6f1a69efc8876a90c960
David Goerger <dgoerger@users.noreply.github.com>
2016-11-29 09:31:30 -0500

    oh every attribute is overridden even if the recipe isn't applied

--- cookbooks/workstation/recipes/dnsmasq.rb
+++ cookbooks/workstation/recipes/dnsmasq.rb
@@ -1,18 +1,25 @@
-if node['workstation']['dnscrypt'] or node['workstation']['dns_management']
-  # if NetworkManager isn't starting dnsmasq, we have to
-  service 'dnsmasq' do
-    supports :status => true, :restart => true
-    action [ :enable, :start ]
+unless node['workstation']['corporate']
+  if node['workstation']['dnscrypt_providers'].any? or node['workstation']['dns_providers'].any?
+    # if NetworkManager isn't starting dnsmasq, we have to
+    service 'dnsmasq' do
+      supports :status => true, :restart => true
+      action [ :enable, :start ]
+    end
+    template '/etc/dnsmasq.d/settings.conf' do
+      source 'dnsmasq.conf.erb'
+      owner 'root'
+      group 'root'
+      mode '0444'
+      action :create
+      notifies :restart, 'service[dnsmasq]', :delayed
+    end
+    include_recipe 'workstation::dnscrypt'
   end
-  template '/etc/dnsmasq.d/settings.conf' do
-    source 'dnsmasq.conf.erb'
-    owner 'root'
-    group 'root'
-    mode '0444'
-    action :create
-    notifies :restart, 'service[dnsmasq]', :delayed
+else
+  # if corporate, delete the dnsmasq conf if it exists
+  file '/etc/dnsmasq.d/settings.conf' do
+    action :delete
   end
-  include_recipe 'workstation::dnscrypt'
 end
 service 'NetworkManager' do
   supports :reload => true
