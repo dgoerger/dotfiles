287afecd094f157ab291905777938044ee0d891f
David Goerger <dgoerger@freeshell.org>
2016-11-29 09:31:30 -0500

    oh every attribute is overridden even if the recipe isn't applied

--- cookbooks/workstation/attributes/corporate.rb
+++ cookbooks/workstation/attributes/corporate.rb
@@ -1,6 +1,1 @@
-# intranets tend to have at least a few old things
-override['workstation']['crypto-policy'] = 'DEFAULT'
-# dnscrypt - not for corporate networks, probably
-override['workstation']['dnscrypt'] = false
-# don't try to override corporate dns policy, default instead from DHCP
-override['workstation']['dns_management'] = false
+default['workstation']['corporate'] = true
