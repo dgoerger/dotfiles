2a62d44f647773e18d08afe417b87901899eb441
David Goerger <dgoerger@users.noreply.github.com>
2016-11-29 20:58:01 -0500

    add system-level knife.rb

--- cookbooks/workstation/files/knife.rb
+++ cookbooks/workstation/files/knife.rb
@@ -4,7 +4,7 @@ log_level                :info
 log_location             STDOUT
 node_name                ENV['OPSCODE_USER'] || ENV['USER']
 client_key               ENV['KNIFE_PATH']
-chef_server_url          "https://chef.its.yale.edu/organizations/yaleits"
+chef_server_url          ENV['CHEF_SERVER']
 ssl_verify_mode          :verify_peer
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.cache/chef-checksums" )
