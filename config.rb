fedbd073ecf76089091052938b10a2667f54a53b
David Goerger <dgoerger@users.noreply.github.com>
2016-11-29 19:27:47 -0500

    avoid config/.rb namespace conflict in top level folder

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
 root = File.expand_path(File.dirname(__FILE__))
 file_cache_path   root + '/cache'
 cookbook_path     root + '/cookbooks'
-role_path         root + '/config/roles'
+role_path         root + '/roles'
