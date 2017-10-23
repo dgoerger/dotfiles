48414ae92b1ddc71bc85b5c2e6f72b7b05f8e9f7
David Goerger <dgoerger@users.noreply.github.com>
2017-10-22 21:19:27 -0400

    deprecate BIND9 and DNSMASQ options for dnsblock, in favour of Unbound

--- scripts/dnsblock.sh
+++ scripts/dnsblock.sh
@@ -2,91 +2,41 @@
 
 UPSTREAM_HOSTS_FILE='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
 
-## paths
-NAMED_DIR='/etc/named'
-NAMED_RPZ_FILE="${NAMED_DIR}/rpz.zone"
-NETWORKMANAGER_DIR='/etc/NetworkManager/dnsmasq.d'
-NETWORKMANAGER_BLOCKLIST_FILE="${NETWORKMANAGER_DIR}/blocklist.conf"
-DNSMASQ_DIR='/etc/dnsmasq.d'
-DNSMASQ_BLOCKLIST_FILE="${DNSMASQ_DIR}/blocklist.conf"
-UNBOUND_DIR='/var/unbound/etc'
-UNBOUND_BLOCKLIST_FILE="${UNBOUND_DIR}/blocklist.conf"
-TMP='/var/tmp'
-SRC="${TMP}/hostfile.src"
-TMP_RPZ="${TMP}/rpz"
-TMP_DNSMASQ="${TMP}/dnsmasq"
-TMP_UNBOUND="${TMP}/unbound"
+TMP='/tmp/unbound'
+SRC='/tmp/hostfile.src'
+CONF_DIR='/usr/local/etc'
+BLOCKLIST_FILE="${CONF_DIR}/blocklist.conf"
 
-
-## functions
-build_for_isc_bind() {
-  # build BIND9 RPZ zone file
-  echo '$TTL 3H' | tee ${TMP_RPZ} >/dev/null 2>&1
-  echo '@                       SOA LOCALHOST. blocked (1 1h 15m 30d 2h)' | tee --append ${TMP_RPZ} >/dev/null 2>&1
-  echo '                        NS  LOCALHOST.' | tee --append ${TMP_RPZ} >/dev/null 2>&1
-  echo '' | tee --append ${TMP_RPZ} >/dev/null 2>&1
-  awk '$1 == "0.0.0.0" {print $2, "CNAME", "."}' ${SRC} | grep -vE "^[0-9].*[0-9] CNAME \.$" | tee --append ${TMP_RPZ} >/dev/null 2>&1
-
-  # copy in the new dnsblock policy
-  if [[ -f ${NAMED_RPZ_FILE} ]]; then
-    cp -p "${NAMED_RPZ_FILE}" "${NAMED_RPZ_FILE}.bak"
-  fi
-  cp "${TMP_RPZ}" "${NAMED_RPZ_FILE}"
-  chown root:named "${NAMED_RPZ_FILE}"
-  chmod 0444 "${NAMED_RPZ_FILE}"
-
-  # load new blocklist
-  if named-checkconf 2>&1; then
-    systemctl restart named
-  else
-    if [[ -f "${NAMED_RPZ_FILE}.bak" ]]; then
-      cp -p "${NAMED_RPZ_FILE}.bak" "${NAMED_RPZ_FILE}"
-    else
-      rm "${NAMED_RPZ_FILE}"
-    fi
-  fi
-}
 
 build_for_unbound() {
-  awk '$1 == "0.0.0.0" {print "local-zone: \""$2"\" static"}' ${SRC} | tee ${TMP_UNBOUND} >/dev/null 2>&1
-  if [[ -f ${UNBOUND_BLOCKLIST_FILE} ]]; then
-    cp -p ${UNBOUND_BLOCKLIST_FILE} ${UNBOUND_BLOCKLIST_FILE}.bak
+  awk '$1 == "0.0.0.0" {print "local-zone: \""$2"\" static"}' ${SRC} | tee ${TMP} >/dev/null 2>&1
+  if [[ -f ${BLOCKLIST_FILE} ]]; then
+    cp -p ${BLOCKLIST_FILE} ${BLOCKLIST_FILE}.bak
   fi
-  cp ${TMP_UNBOUND} ${UNBOUND_BLOCKLIST_FILE}
-  chown root:_unbound ${UNBOUND_BLOCKLIST_FILE}
-  chmod 0444 ${UNBOUND_BLOCKLIST_FILE}
+  mkdir -p ${CONF_DIR}
+  cp ${TMP} ${BLOCKLIST_FILE}
+  chmod 0444 ${BLOCKLIST_FILE}
   if /usr/sbin/unbound-checkconf >/dev/null 2>&1; then
-    /etc/rc.d/unbound restart
-  elif [[ -f ${UNBOUND_BLOCKLIST_FILE}.bak ]]; then
-    mv ${UNBOUND_BLOCKLIST_FILE}.bak ${UNBOUND_BLOCKLIST_FILE}
+    if [[ "$(uname)" == 'OpenBSD' ]]; then
+      /etc/rc.d/unbound restart
+    fi
+  elif [[ -f ${BLOCKLIST_FILE}.bak ]]; then
+    mv ${BLOCKLIST_FILE}.bak ${BLOCKLIST_FILE}
   fi
-}
-
-build_for_dnsmasq() {
-  # build for dnsmasq
-  echo "# ipv4" | tee ${TMP_DNSMASQ} >/dev/null 2>&1
-  awk '$1 == "0.0.0.0" {print "address=\"/" $2 "/0.0.0.0\""}' ${SRC} | tee --append ${TMP_DNSMASQ} >/dev/null 2>&1
-  echo "# ipv6" | tee --append ${TMP_DNSMASQ} >/dev/null 2>&1
-  awk '$1 == "0.0.0.0" {print "address=\"/" $2 "/::\""}' ${SRC} | tee --append ${TMP_DNSMASQ} >/dev/null 2>&1
+  rm ${TMP} ${SRC}
 }
 
 
 ## main
 if curl -Lo ${SRC} ${UPSTREAM_HOSTS_FILE} 2>/dev/null; then
-  # ISC BIND9
+  # unbound
   if pgrep unbound >/dev/null 2>&1; then
     build_for_unbound
-  elif systemctl status named >/dev/null 2>&1; then
-    build_for_isc_bind
-  # NetworkManager's built-in support for dnsmasq
-  elif systemctl status NetworkManager >/dev/null 2>&1; then
-    build_for_dnsmasq
-    cp ${TMP_DNSMASQ} ${NETWORKMANAGER_BLOCKLIST_FILE}
-    chmod 0444 ${NETWORKMANAGER_BLOCKLIST_FILE}
-  elif systemctl status dnsmasq >/dev/null 2>&1; then
-    build_for_dnsmasq
-    cp ${TMP_DNSMASQ} ${DNSMASQ_BLOCKLIST_FILE}
-    chmod 0444 ${DNSMASQ_BLOCKLIST_FILE}
-    systemctl restart dnsmasq
+  else
+    echo "ERROR: Unbound not running."
+    exit 1
   fi
+else
+  echo "ERROR: Upstream blocklist unreachable."
+  exit 1
 fi
