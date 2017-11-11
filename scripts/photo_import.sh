1c77ba9795fa4a902807ece752351cb768ab9285
David Goerger <dgoerger@freeshell.org>
2017-11-10 21:01:41 -0500

    photo_import is.. almost posix-compliant

--- scripts/photo_import.sh
+++ scripts/photo_import.sh
@@ -1,79 +1,52 @@
-#!/bin/bash
-###############################################
-# adapted from: https://mikebeach.org/?p=4729 #
-###############################################
+#!/bin/sh
+#
+# The script will search recursively for
+# exif-supported filetypes in the current
+# directory, and copy files to
+# ${MOVETO}/\$YYYY/\$MM/\$DD
 
-# EXIF data is only present in jpeg and tiff
-# generate import failure log for other types
-# TODO: properly import other types
-FILETYPES="jpg jpeg tif tiff png gif bmp xcf ogg ogv webm mp4 mov"
+# TODO properly import other types
+FILETYPES="jpg jpeg tif tiff"
 
-# CHANGEME: destination folder
-MOVETO="${HOME}/Pictures/"
-
-# sanity check that $MOVETO exists
-if [[ ! -d "${MOVETO}" ]]; then
-  echo "Abort! Destination ${MOVETO} does not exist!"
+# require exif
+if [ ! -x "$(which exif 2>/dev/null)" ]; then
+  echo "Abort! Depends on exif."
   exit 1
 fi
 
-# print help/usage info
-if [[ "${1}" == "-h" || "${1}" == "--help" || "${1}" == "--usage" ]]; then
-  echo ""
-  echo "Usage:"
-  echo "  sh script"
-  echo ""
-  echo "The script will search recursively for"
-#  echo "filetypes: $(echo ${FILETYPES} | sed 's/\ /\,\ /g')"
-  echo "filetypes: ${FILETYPES//\ /\,\ /g}"
-  echo "in the current directory, and copy files"
-  echo "containing valid EXIF data to:"
-  echo "${MOVETO}\$YYYY/\$MM/\$DD"
-  echo ""
-  exit
-fi
+import_photo() {
+  MOVETO="${HOME}/Pictures"
 
-###############################################
-## nested execution call to perform the sort ##
-###############################################
-# invoked when the programs calls itself with
-# $1 = "doAction"
-# $2 = <file to handle>
-
-# SKIP IF NOT CALLED BY SELF
-if [[ "${1}" == "doAction" && "${2}" != "" ]]; then
   # fetch EXIF data
-  DATETIME=$(exif -t 'DateTime' -m "${2}" 2>/dev/null | awk -F" " '{print $1}' | sed 's/\:/\//g' | sort -u)
-  if [[ "${DATETIME}" == "" ]]; then
-    echo "${2}: Abort! DateTime not found" | tee --append "${MOVETO}/$(date +%Y%m%d-%H%M)_import_failure.log"
+  DATETIME="$(exif -t 'DateTime' -m "${1}" 2>/dev/null | awk -F" " '{print $1}' | sed 's/\:/\//g' | sort -u)"
+
+  # sanity checks re datetime data
+  if [ -z "${DATETIME}" ]; then
+    echo "${1}: Abort! DateTime not found" | tee --append "${MOVETO}/$(date +%Y%m%d-%H%M)_import_failure.log"
     exit 1;
   fi;
-  if [[ "$(echo "${DATETIME}" | wc -l)" != "1" ]]; then
-    echo "${2}: Abort! File has more than one DateTime declaration" | tee --append "${MOVETO}/$(date +%Y%m%d-%H%M)_import_failure.log"
+  if [ "$(echo "${DATETIME}" | wc -l)" != "1" ]; then
+    echo "${1}: Abort! File has more than one DateTime declaration" | tee --append "${MOVETO}/$(date +%Y%m%d-%H%M)_import_failure.log"
     exit 1;
   fi
-  # lowercase filename and use the current extension as-is
-  FILENAME=$(echo "${2}" | awk -F"/" '{print $NF}' | tr '[:upper:]' '[:lower:]')
-  # copy is safer than move
-  mkdir -p "${MOVETO}/${DATETIME}" && cp -np "${2}" "${MOVETO}/${DATETIME}/${FILENAME}"
-  echo ": success!"
-  exit
-fi;
 
-##############################################
-###### loop to find pictures and videos ######
-##############################################
+  # lowercase the filename
+  FILENAME="$(echo "${1}" | awk -F"/" '{print $NF}' | tr '[:upper:]' '[:lower:]')"
+
+  if [ -n "${FILENAME}" ]; then
+    # copy is safer than move
+    mkdir -p "${MOVETO}/${DATETIME}"
+    if [ ! -f "${MOVETO}/${DATETIME}/${FILENAME}" ]; then
+      cp -p "${1}" "${MOVETO}/${DATETIME}/${FILENAME}" && echo "Imported ${DATETIME}/${FILENAME}"
+    fi
+  fi
+}
+
+# main
 for x in ${FILETYPES}; do
-  # check for exif command
-  EXIF=$(which exif)
-  if [ "${EXIF}" == "" ]; then
-    echo "Abort! The 'exif' command is missing or unavailable"
-    exit 1
-  fi;
-  # locate and process found images
-  find . -iname "*.${x}" -print0 -exec sh -c "$0 doAction '{}'" \;
+  # WARNING: 'typeset' is NOT posix,
+  #          but works in bash and ksh
+  find . -iname "*.${x}" -exec sh -c "
+    $(typeset -f import_photo)"'
+    import_photo "$@"' sh {} \;
 done
-
-# clean up empty dirs and dbs
-find "${MOVETO}" -name "Thumbs.db" -delete
-find "${MOVETO}" -empty -delete
