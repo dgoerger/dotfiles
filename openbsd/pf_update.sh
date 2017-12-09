2fcf06de918988c68a4c63922022576bcc485422
David Goerger <dgoerger@users.noreply.github.com>
2017-12-09 13:54:20 -0500

    pf_update should also ban ssh bruteforcers

--- openbsd/pf_update.sh
+++ openbsd/pf_update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+#
+# pf_update
+#   Blocks nasties at the system firewall.
+#
 
+## Emerging Threats ruleset
 DROP_FILE='pf.et.conf'
 PF_MAIN='/etc/pf.conf'
 PF_CONF_DIR='/usr/local/etc'
@@ -7,16 +12,14 @@ PF_DROP_CONF="${PF_CONF_DIR}/${DROP_FILE}"
 TMP_FILE="/tmp/${DROP_FILE}"
 SOURCE='https://rules.emergingthreats.net/fwrules/emerging-PF-ALL.rules'
 
-# crontab doesn't necessarily have a full path
-PATH=${PATH}:/usr/local/bin:/usr/local/sbin
-export PATH
-
-if [ ! -f "${PF_MAIN}" ]; then
+# sanity check
+if [ ! -f "${PF_MAIN}" ] || [ ! -x "$(which pfctl 2>/dev/null)" ]; then
   echo 'This script only supports pf.'
   exit 1
 fi
 
-if curl -Lo "${TMP_FILE}" "${SOURCE}" 2>/dev/null; then
+# fetch updated ruleset
+if /usr/local/bin/curl -Lo "${TMP_FILE}" "${SOURCE}" 2>/dev/null; then
   if pfctl -nf "${TMP_FILE}" 2>/dev/null; then
     mkdir -p "${PF_CONF_DIR}"
     if [ -f "${PF_DROP_CONF}" ]; then
@@ -34,3 +37,14 @@ if curl -Lo "${TMP_FILE}" "${SOURCE}" 2>/dev/null; then
 else
   echo 'Please verify the Emerging Threats upstream download URL and that cURL is installed.' | mail -s 'pf: failed to download ET droplist' root
 fi
+
+## scan for bruteforcers in /var/log/authlog AND BLOCK
+# TODO ipv6
+# block anyone trying to auth to system accounts
+awk '/^[1-9].*[0-9]$/ /Disconnecting authenticating user (root|daemon|operator|bin|build|sshd|www|nobody|_).*Too many authentication failures/ {print $10}' /var/log/authlog | sort -u | while read -r ip; do
+  pfctl -t bruteforce -T add "${ip}"
+done
+# block IPs with repeated invalid username login attempts (n>2)
+awk '/^[1-9].*[0-9]$/ /Disconnecting invalid user.*Too many authentication failures/ {print $10}' /var/log/authlog | sort | uniq -c | awk '$1 > 2 {print $2}' | while read -r ip; do
+  pfctl -t bruteforce -T add "${ip}"
+done
