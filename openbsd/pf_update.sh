63ca823e0cab8d532d54eccba8089aa34a5bbc29
David Goerger <dgoerger@freeshell.org>
2018-01-16 20:47:50 -0500

    split out bruteforcer scripts from main pf_update script so that they can be run on different schedules

--- openbsd/pf_update.sh
+++ openbsd/pf_update.sh
@@ -37,23 +37,3 @@ if ftp -VMo "${TMP_FILE}" "${SOURCE}" 2>/dev/null; then
 else
   echo 'Please verify the Emerging Threats upstream download URL.' | mail -s 'pf: failed to download ET droplist' root
 fi
-
-## scour ssh logs for bruteforcers AND BLOCK
-# TODO ipv6
-# block anyone trying to auth to system accounts
-awk '/^[1-9].*[0-9]$/ /Disconnecting authenticating user (root|daemon|operator|bin|build|sshd|www|nobody|_).*Too many authentication failures/ {print $10}' /var/log/authlog | sort -u | while read -r ip; do
-  pfctl -t bruteforce -T add "${ip}" >/dev/null 2>&1
-done
-# block IPs with repeated invalid username login attempts (n>2)
-awk '/^[1-9].*[0-9]$/ /Disconnecting invalid user.*Too many authentication failures/ {print $10}' /var/log/authlog | sort | uniq -c | awk '$1 > 2 {print $2}' | while read -r ip; do
-  pfctl -t bruteforce -T add "${ip}" >/dev/null 2>&1
-done
-# block ssh port scanners who don't even try to log in
-awk '/^[1-9].*[0-9]$/ /sshd.*Connection closed by [1-9].*\[preauth\]/ {print $9}' /var/log/authlog | sort | uniq -c | awk '$1 > 2 {print $2}' | while read -r ip; do
-  pfctl -t bruteforce -T add "${ip}" >/dev/null 2>&1
-done
-
-## scan for obvious spammers and mxsploiters AND BLOCK
-awk '/smtp event=failed-command.*command="AUTH LOGIN"/ {gsub ("address=","",$9); print $9}' /var/log/maillog | sort -u | while read -r ip; do
-  pfctl -t bruteforce -T add "${ip}" >/dev/null 2>&1
-done
