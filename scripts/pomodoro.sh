3efa6186988282a69f1e0a734106cfa62995a14d
David Goerger <dgoerger@freeshell.org>
2018-02-10 21:03:06 -0500

    pomodoro hadn't been portable in the cli anyway; require DESKTOP_SESSION

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -3,20 +3,25 @@
 usage="Usage:\
   pomodoro [minutes] [message]\
 "
-if [ $# -ne 2 ]; then
+
+# sanity checks
+if [ ! -x "$(/usr/bin/which notify-send 2>/dev/null)" ] || [ -n "${DESKTOP_SESSION}" ]; then
+  echo 'This only works in a graphical environment.'
+  return 1
+elif [ ! -x "$(/usr/bin/which tmux 2>/dev/null)" ]; then
+  echo 'Please install tmux.'
+  return 1
+elif [ $# -ne 2 ]; then
   echo "${usage}"
   return 1
 else
   message="${2}"
 fi
+
 case ${1} in
-  ''|*[!0-9]*) echo "Error: \${1} must be an integer." && exit 1 ;;
+  ''|*[!0-9]*) echo "Error: \${1} must be an integer." && return 1 ;;
   *) delay=${1} ;;
 esac
-if [ -n "${DESKTOP_SESSION}" ] && [ -x /usr/bin/notify-send ]; then
-  # if running a desktop with Linux DBus available       
-  /usr/bin/tmux new -d "sleep $(echo "${delay}*60" | /usr/bin/bc -l); /usr/bin/notify-send POMODORO ${message} --icon=dialog-warning-symbolic --urgency=critical"
-else
-  # otherwise assume we've a remote shell                
-  /usr/bin/tmux new -d "sleep $(echo "${delay}*60" | /usr/bin/bc -l); echo ${message} | wall -g $(id -ng)"
-fi
+
+# main
+/usr/bin/tmux new -d "sleep $(echo "${delay}*60" | bc -l); notify-send POMODORO ${message} --icon=dialog-warning-symbolic --urgency=critical"
