fe1987a4af7c5cb6502d803a46ba27bf4032b0ea
David Goerger <dgoerger@freeshell.org>
2018-09-07 22:25:01 -0400

    add basic privsep to pf_blocklist.sh

--- bin/pf_blocklist.sh
+++ bin/pf_blocklist.sh
@@ -1,30 +1,30 @@
 #!/bin/ksh
 
-TMPDIR="$(mktemp -d)"
-TMPFILE="$(mktemp)"
+TMPFILE="$(mktemp -t pf.XXXXXX)"
 CONF="/usr/local/etc/pf_blocklist.conf"
+chown root:_pkgfetch "${TMPFILE}"
+chmod 0660 "${TMPFILE}"
 
-case "$(uname)" in
-  OpenBSD) FETCH='/usr/bin/ftp -VM' ;;
-  *) echo 'ERROR: Unsupported OS' && return 1 ;;
-esac
+if [[ "$(uname)" != 'OpenBSD' ]]; then
+  echo 'ERROR: Unsupported OS' && return 1
+fi
+
+# kludge to avoid a race condition with the dnsblock script reloading unbound
+sleep 90
 
 # download IP blocklists and parse
-cd "${TMPDIR}"
-${FETCH} https://www.binarydefense.com/banlist.txt
-${FETCH} https://rules.emergingthreats.net/blockrules/compromised-ips.txt
-${FETCH} https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_1d.netset
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_30d.netset
-${FETCH} https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_proxies.netset
-awk '/^[1-9].*[0-9]$/ {print $1}' *.txt *.netset | sort -uV > "${TMPFILE}"
+su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/bds_atif.ipset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/et_block.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/et_compromised.ipset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_1d.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_30d.netset \
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_proxies.netset | awk '/^[1-9].*[0-9]$/ {print $1}' | sort -uV > ${TMPFILE}"
 
 # block Shodan
-${FETCH} https://isc.sans.edu/api/threatlist/shodan/shodan.txt 
-grep -Eo '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' shodan.txt >> "${TMPFILE}"
+su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://isc.sans.edu/api/threatlist/shodan/shodan.txt | grep -Eo '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' >> ${TMPFILE}"
 
 # copy into place
 if [[ -f "${CONF}" ]]; then
