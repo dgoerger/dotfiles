f7b007a7dd8aea1d9bc26ec206f0572f46386e05
David Goerger <dgoerger@freeshell.org>
2018-11-20 21:23:55 -0500

    sync

--- bin/pf_blocklist.sh
+++ bin/pf_blocklist.sh
@@ -13,18 +13,14 @@ fi
 sleep 90
 
 # download IP blocklists and parse
-su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/bds_atif.ipset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/et_block.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/et_compromised.ipset \
+/usr/bin/su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://www.binarydefense.com/banlist.txt \
+https://rules.emergingthreats.net/blockrules/compromised-ips.txt \
+https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt \
 https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_1d.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_abusers_30d.netset \
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_proxies.netset | awk '/^[1-9].*[0-9]$/ {print $1}' | sort -uV > ${TMPFILE}"
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset | awk '/^[1-9].*[0-9]$/ {print $1}' | sort -uV > ${TMPFILE}"
 
 # block Shodan
-su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://isc.sans.edu/api/threatlist/shodan/shodan.txt | grep -Eo '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' >> ${TMPFILE}"
+/usr/bin/su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://isc.sans.edu/api/threatlist/shodan/shodan.txt | grep -Eo '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' >> ${TMPFILE}"
 
 # copy into place
 if [[ -f "${CONF}" ]]; then
