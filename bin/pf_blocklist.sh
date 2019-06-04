39f8ca5b3deeeab330edfd77a0bcbd53c9fb4a4d
David Goerger <dgoerger@freeshell.org>
2019-06-03 22:39:30 -0400

    sync

--- bin/pf_blocklist.sh
+++ bin/pf_blocklist.sh
@@ -22,8 +22,8 @@ https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2
 # block Shodan
 /usr/bin/su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - https://isc.sans.edu/api/threatlist/shodan/shodan.txt | grep -Eo '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' >> ${TMPFILE}"
 
-# nixspam.org
-/usr/bin/su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - http://www.dnsbl.manitu.net/download/nixspam-ip.dump.gz | /usr/bin/gzcat | grep -Eo '\ [1-9].*[0-9]' | sed 's/\ //g' | sort >> ${TMPFILE}"
+## nixspam.org
+#/usr/bin/su -s/bin/sh _pkgfetch -c "/usr/bin/ftp -VMo - http://www.dnsbl.manitu.net/download/nixspam-ip.dump.gz | /usr/bin/gzcat | grep -Eo '\ [1-9].*[0-9]' | sed 's/\ //g' | sort >> ${TMPFILE}"
 
 # copy into place
 if [[ -f "${CONF}" ]]; then
@@ -31,7 +31,7 @@ if [[ -f "${CONF}" ]]; then
 fi
 cp "${TMPFILE}" "${CONF}"
 chown root:wheel "${CONF}"
-chmod 0444 "${CONF}"
+chmod 0440 "${CONF}"
 
 # verify syntax and reload pf
 if pfctl -nf /etc/pf.conf 2>/dev/null; then
