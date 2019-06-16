7669afeea87a2cd009c60486ae7f2d94f8fb16a2
David Goerger <dgoerger@freeshell.org>
2019-06-16 13:01:55 -0400

    add ports(7) and X(7) to manuals tab-completion

--- bin/manuals_tab_completion.sh
+++ bin/manuals_tab_completion.sh
@@ -10,26 +10,25 @@
 #   - the list of manuals doesn't update until the next cron(8) run
 #   - many manuals in section 1 don't correspond with an executable
 #     and are thus filtered out (e.g. perl* and git-*)
-#   - X11 manuals are omitted
-#   - sections 5 and 7 completions omit manuals from ports(7)
 #   - sections 2, 3, 3p, 4, and 9 are omitted for brevity
 
 TMPFILE="$(mktemp)"
 CONFFILE="/usr/local/etc/manuals.list"
 
 if [[ "$(uname)" == 'OpenBSD' ]]; then
+  export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/games:/usr/local/bin:/usr/X11R6/bin
   # why is section 1 filled with so much junk?
-  man -M /usr/share/man:/usr/local/man -s 1 -k Nm~. | cut -d\( -f1 | tr -d , | while read -r COMMAND; do if [[ -x "$(/usr/bin/which "${COMMAND}" 2>/dev/null)" ]]; then echo "${COMMAND}"; fi; done >> "${TMPFILE}"
-  #man -M /usr/share/man:/usr/local/man -s 2 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  #man -M /usr/share/man:/usr/local/man -s 3 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  #man -M /usr/share/man:/usr/local/man -s 3p -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  #man -M /usr/share/man:/usr/local/man -s 4 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  man -M /usr/share/man -s 5 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  man -M /usr/share/man:/usr/local/man -s 6 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  man -M /usr/share/man -s 7 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  man -M /usr/share/man:/usr/local/man -s 8 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  #man -M /usr/share/man:/usr/local/man -s 9 -k Nm~. | cut -d\( -f1 | tr -d , >> "${TMPFILE}"
-  cat "${TMPFILE}" | sort -u > "${CONFFILE}"
+  man -M /usr/share/man:/usr/local/man:/usr/X11R6/man -s 1 -k Nm~. | cut -d\( -f1 | while read -r COMMAND; do if [[ -x "$(/usr/bin/which "${COMMAND}" 2>/dev/null)" ]]; then echo "${COMMAND}"; fi; done >> "${TMPFILE}"
+  #man -M /usr/share/man -s 2 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  #man -M /usr/share/man:/usr/local/man:/usr/X11R6/man -s 3 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  #man -M /usr/share/man:/usr/local/man -s 3p -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  #man -M /usr/share/man:/usr/X11R6/man -s 4 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  man -M /usr/share/man:/usr/local/man:/usr/X11R6/man -s 5 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  man -M /usr/share/man:/usr/local/man -s 6 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  man -M /usr/share/man:/usr/local/man:/usr/X11R6/man -s 7 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  man -M /usr/share/man:/usr/local/man -s 8 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  #man -M /usr/share/man -s 9 -k Nm~. | cut -d\( -f1 >> "${TMPFILE}"
+  cat "${TMPFILE}" | tr -d , | tr '[:space:]' '\n' | awk '/^[a-zA-Z]/' | sort -u > "${CONFFILE}"
   chown root:wheel "${CONFFILE}"
   chmod 0444 "${CONFFILE}"
 fi
