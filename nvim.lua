32641d05e195e2952e1124ffc68c16f8e311ea27
David Goerger <dgoerger@freeshell.org>
2022-09-02 14:24:29 +0000

    enable syntax highlighting for jenkinsfiles (groovy)

--- nvim.lua
+++ nvim.lua
@@ -18,8 +18,9 @@ set.spelllang   = 'en_ca'       -- enable spellcheck with ':set spell'
 
 -- indentation schema
 vim.cmd [[
-        set nofoldenable
+	set nofoldenable
 	autocmd BufNewFile,BufRead .calendar set ft=calendar noexpandtab
 	autocmd BufRead,BufNewFile /tmp/mutt* set ft=mail spell formatoptions+=aw nosmartindent nocindent indentexpr=
 	autocmd BufRead,BufNewFile *.md,*.markdown set ft=mkd syntax=markdown spell formatoptions+=aw textwidth=70 nosmartindent nocindent indentexpr=
+	autocmd BufRead,BufNewFile Jenkinsfile*,*.jenkinsfile set ft=groovy syntax=groovy
 ]]
