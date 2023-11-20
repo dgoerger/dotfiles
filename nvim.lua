0d1f35c42e0f778547c035d5ee6dbdacc66730be
David Goerger <dgoerger@freeshell.org>
2023-11-20 12:02:46 +0000

    enable truecolour support in neovim

--- nvim.lua
+++ nvim.lua
@@ -1,20 +1,21 @@
 -- ~/.config/nvim/init.lua
 -- docs: nvim's built-in ':help option' documentation system
 
-local set       = vim.opt
+local set         = vim.opt
 
 vim.cmd [[
 	colorscheme elflord
 ]]
 
-set.expandtab	= true          -- use spaces by default instead of tabs
-set.ignorecase  = true          -- ignore case when searching
-set.incsearch   = true          -- search as you type
-set.modelines   = 0             -- disable modelines for security, see rhbz#1398227
-set.mouse       = ''            -- disable mouse support (jumpy trackpads)
-set.shada       = ''            -- disable history file
-set.smartcase   = true          -- strict case searching CAPS
-set.spelllang   = 'en_ca'       -- enable spellcheck with ':set spell'
+set.expandtab     = true        -- use spaces by default instead of tabs
+set.ignorecase    = true        -- ignore case when searching
+set.incsearch     = true        -- search as you type
+set.modelines     = 0           -- disable modelines for security, see rhbz#1398227
+set.mouse         = ''          -- disable mouse support (jumpy trackpads)
+set.shada         = ''          -- disable history file
+set.smartcase     = true        -- strict case searching CAPS
+set.spelllang     = 'en_ca'     -- enable spellcheck with ':set spell'
+set.termguicolors = true        -- enable truecolour support
 
 -- indentation schema
 vim.cmd [[
