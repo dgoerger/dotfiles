30ad8fe1b93d99afc43ab34a25caba6341b08115
David Goerger <dgoerger@freeshell.org>
2023-12-19 01:29:06 +0000

    add sysstats script for gathering system stats via cron

---
+++ bin/sysstats.sh
@@ -0,0 +1,26 @@
+#!/bin/ksh -
+set -Cfuo pipefail
+
+umask 0027
+
+SYSSTATS_DIR="/var/log/sysstats"; readonly SYSSTATS_DIR
+DATETIME="$(date +%Y%m%d_%H%M)"; readonly DATETIME
+
+if [[ ! -d "${SYSSTATS_DIR}" ]]; then
+	mkdir -pm 0750 "${SYSSTATS_DIR}"
+	chown root:wheel "${SYSSTATS_DIR}"
+fi
+
+# stats
+df -lP | awk '/^\/dev/ {print $NF}' | while read -r FILESYSTEM; do
+	fstat -f "${FILESYSTEM}" | grep -Ev "^USER"; done \
+	| gzip > "${SYSSTATS_DIR}/${DATETIME}_fstat.gz"
+
+netstat -an \
+	| gzip > "${SYSSTATS_DIR}/${DATETIME}_netstat.gz"
+
+ps -Awwfo user,pid,ppid,pgid,lstart,%cpu,%mem,stat,wchan,command \
+	| gzip > "${SYSSTATS_DIR}/${DATETIME}_pstree.gz"
+
+# cleanup
+find "${SYSSTATS_DIR}" -type f -mtime +7 -delete
