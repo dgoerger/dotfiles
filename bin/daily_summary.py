1fd1d1bb3bfd9584cf2d1c5e1d64e0a81d707c8e
David Goerger <dgoerger@freeshell.org>
2024-07-12 20:29:31 -0400

    add UV index info to current weather conditions
    Extend the current weather conditions section to include the UV index value as forecast by the US Environmental Protection Agency.
    
    While here, shorten output outside of the morning report and midday check-ins, and implement a one-level fallback to the next-nearest National Weather Service observation station if the nearest one is offline at the time of query. If both stations are offline, assert that the current temperature is absolute zero (-273 Kelvin) so that it will be obvious we're operating with bad data.
    
    ```
    AQI: 77 (Moderate, OZONE); UV: 4
    Feels like: 29ºC (27ºC, 79% humidity)
    ```
    
    Change-Id: I83e90eca3091fff925d432a73274fe11b54e82a6
    Reviewed-on: https://review.daemonica.net/c/dgoerger/dotfiles/+/142
    Reviewed-by: David Goerger <dgoerger@daemonica.net>

--- bin/daily_summary.py
+++ bin/daily_summary.py
@@ -29,6 +29,10 @@ def argparse(args: list[str]) -> list[str]:
     return [latitude, longitude]
 
 
+def hour_of_day() -> int:
+    return int(strftime("%k"))
+
+
 def today() -> str:
     return " ".join(strftime("%A, %e %B %Y").split())
 
@@ -55,6 +59,7 @@ def geo_query(latitude: str, longitude: str) -> dict[str, str]:
     geo_data = {
         "forecast_url": geo_json["forecast"] + "?units=si",
         "observation_stations_url": geo_json["observationStations"],
+        "city": geo_json["relativeLocation"]["properties"]["city"],
         "state": geo_json["relativeLocation"]["properties"]["state"],
     }
     return geo_data
@@ -75,14 +80,28 @@ def current_weather(observation_stations_url: str) -> dict[str, int]:
         current_conditions_json = json.loads(current_conditions_response.read())[
             "properties"
         ]
+    if current_conditions_json["temperature"]["value"] is None:
+        # fall back to the next-nearest station if the closest weather station
+        # .. doesn't return valid data, for example during station maintenance
+        stationIdentifier = stations_json[1]["properties"]["stationIdentifier"]
+        with urllib.request.urlopen(
+            urllib.request.Request(current_conditions_url)
+        ) as current_conditions_response:
+            current_conditions_json = json.loads(current_conditions_response.read())[
+                "properties"
+            ]
+
     apparent_temperature = (
         current_conditions_json["windChill"]["value"]
         or current_conditions_json["heatIndex"]["value"]
         or current_conditions_json["temperature"]["value"]
+        or -273
     )
     current_data = {
-        "temperature": round(current_conditions_json["temperature"]["value"]),
-        "relativeHumidity": round(current_conditions_json["relativeHumidity"]["value"]),
+        "temperature": round(current_conditions_json["temperature"]["value"] or -273),
+        "relativeHumidity": round(
+            current_conditions_json["relativeHumidity"]["value"] or 0
+        ),
         "apparent_temperature": round(apparent_temperature),
     }
     return current_data
@@ -105,8 +124,7 @@ def air_quality(latitude: str, longitude: str, geo_data: dict[str, str]) -> str:
         aqi_json = json.loads(aqi_response.read())[0]
 
     aqi = (
-        "Air Quality: "
-        + str(aqi_json["aqi"])
+        str(aqi_json["aqi"])
         + " ("
         + aqi_json["category"]
         + ", "
@@ -116,7 +134,29 @@ def air_quality(latitude: str, longitude: str, geo_data: dict[str, str]) -> str:
     return str(aqi)
 
 
-def weather_forecast(forecast_url: str) -> list[str]:
+def uv_index(geo_data: dict[str, str], hour_of_day: int) -> str:
+    uv_index_url = (
+        "https://data.epa.gov/efservice/getEnvirofactsUVHOURLY/CITY/"
+        + urllib.parse.quote(str(geo_data["city"]))
+        + "/STATE/"
+        + urllib.parse.quote(str(geo_data["state"]))
+        + "/json"
+    )
+    # index 0 starts at 7am? pretend that any time between 00-07 is
+    # .. really 7h, it's likely all 0 anyway. The alternative would
+    # .. be to query the previous day's data, as each day's dataset
+    # .. appears to run from 7h -> 3h the next day, which would
+    # .. still leave a gap between 3h and 7h in any case. It's a bit
+    # .. gross, but c'est la vie.
+    uv_json_index = hour_of_day - 7
+    if uv_json_index < 0:
+        uv_json_index = 0
+    with urllib.request.urlopen(urllib.request.Request(uv_index_url)) as uv_response:
+        uv_json = json.loads(uv_response.read())[uv_json_index]
+    return str(uv_json["UV_VALUE"] or 0)
+
+
+def weather_forecast(forecast_url: str, forecast_lookahead_in_days: int) -> list[str]:
     with urllib.request.urlopen(
         urllib.request.Request(forecast_url)
     ) as forecast_response:
@@ -124,23 +164,41 @@ def weather_forecast(forecast_url: str) -> list[str]:
 
     return [
         f"{forecast_json[index]['name']}: {forecast_json[index]['detailedForecast']}"
-        for index in range(6)
+        for index in range(forecast_lookahead_in_days * 2)
     ]
 
 
 if __name__ == "__main__":
     latitude, longitude = argparse(sys.argv)
-    print(today())
-    print(calendar())
-    print("~~~")
-    print(sunstat(latitude, longitude).rstrip("\n"))
-    print("~~~\n")
+    long_forecast = [6]
+    short_forecast = [12, 18]
+    if hour_of_day() in long_forecast:
+        forecast_lookahead_in_days = 3
+        print(today())
+        print(calendar())
+        print("~~~")
+        print(sunstat(latitude, longitude).rstrip("\n"))
+        print("~~~\n")
+    elif hour_of_day() in short_forecast:
+        forecast_lookahead_in_days = 1
+    else:
+        forecast_lookahead_in_days = 0
     geo_data = geo_query(latitude, longitude)
-    print(air_quality(latitude, longitude, geo_data))
+    print(
+        "AQI: "
+        + air_quality(latitude, longitude, geo_data)
+        + "; UV: "
+        + uv_index(geo_data, hour_of_day())
+    )
     current_data = current_weather(geo_data["observation_stations_url"])
     print(
         f"Feels like: {current_data['apparent_temperature']}ºC "
         f"({current_data['temperature']}ºC, "
         f"{current_data['relativeHumidity']}% humidity)\n"
     )
-    print("\n\n".join(weather_forecast(geo_data["forecast_url"])))
+    if hour_of_day() in (long_forecast or short_forecast):
+        print(
+            "\n\n".join(
+                weather_forecast(geo_data["forecast_url"], forecast_lookahead_in_days)
+            )
+        )
