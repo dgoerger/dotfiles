6f4f818f8aae52c10f78b175f0b275a3339e45ec
David Goerger <dgoerger@daemonica.net>
2024-08-24 09:03:53 -0400

    import make script to build html versions of manual pages
    Tested against both OpenBSD and Alpine Linux manuals. Upload the resulting html files to a web server, and then they can be scraped into a `.zim` file for offline browsing in Kiwix (Wikimedia project at kiwix.org) using something like
    
    ```
    mkdir output
    podman run -v $(pwd)/output:/output ghcr.io/openzim/zimit zimit \
    --keep \
    --workers 4 \
    --url "https://CHANGEME" \
    --title "CHANGEME" \
    --description "CHANGEME" \
    --name "CHANGEME.zim"
    ```
    
    via https://github.com/openzim/zimit
    
    Change-Id: Id4dccb8ba83c835fe3afa080903459deca98685e
    Reviewed-on: https://review.daemonica.net/c/dgoerger/dotfiles/+/382
    Reviewed-by: David Goerger <dgoerger@daemonica.net>

---
+++ manuals/make_mandoc_html.sh
@@ -0,0 +1,17 @@
+#!/bin/ksh -
+set -Cefuo pipefail
+
+mkdir -m 755 outdir
+if [[ -r /usr/share/misc/mandoc.css ]]; then
+        cp /usr/share/misc/mandoc.css mandoc.css
+        ln mandoc.css outdir/mandoc.css
+fi
+
+for i in $(jot 9); do
+        SECTION="/usr/share/man/man${i}"
+        /bin/ls "${SECTION}" | while read -r FILE; do
+                if [[ -f "${SECTION}/${FILE}" ]]; then
+                        mandoc -T html -O style=mandoc.css "${SECTION}/${FILE}" > "outdir/${FILE%.gz}.html"
+                fi
+        done
+done
