af8fdadfac24b9968cae6d2a5d42adc970974f1a
David Goerger <dgoerger@daemonica.net>
2024-09-07 17:31:07 -0400

    replace firehol with upstream sources
    The FireHOL project seems to be mostly on autopilot and unmaintained. After reviewing upstream sources, keep the ones which still appear to be maintained and blocking anything worth blocking. Some of the sources were either blocking no IPs, or only 1, or have not been updated in some time.
    
    The bogons list has been disrecommended by the IETF since 2011, after which point all IPv4 address space really has been allocated. It is not really clear upon what basis FireHOL was continuing to block formerly unallocated space primarily now assigned to AFRINIC and APNIC.
    
    Add Greensnow and Blocklist.de's "bots," "sip," and "strongips" lists from FireHOL Level 2, and StopForumSpam from the abusers list. The "apache" blocklist from Blocklist.de also looks of reasonable quality, but results in too large of a blocklist for my pf to load at this time. Increasing the `table-entries` limit in pf.conf didn't resolve the issue, which should be revisited at a later time.
    
    review prompted by commentary at https://news.ycombinator.com/item?id=41471799
    
    Change-Id: I7e74f5706c4e925b90850e06681a423a5519616d
    Reviewed-on: https://review.daemonica.net/c/dgoerger/dotfiles/+/447
    Reviewed-by: David Goerger <dgoerger@daemonica.net>

--- bin/pf_blocklist.sh
+++ bin/pf_blocklist.sh
@@ -9,13 +9,29 @@ fi
 CONF="/etc/pf.conf.deny"; readonly CONF
 TMPFILE="$(mktemp -t pf.XXXXXX)"; readonly TMPFILE
 
+# sources
+BLOCKLISTDE_BOTS='https://lists.blocklist.de/lists/bots.txt'
+BLOCKLISTDE_LONGTERM='http://lists.blocklist.de/lists/strongips.txt'
+BLOCKLISTDE_TELEPHONY='http://lists.blocklist.de/lists/sip.txt'
+DSHIELD_TOP20='https://feeds.dshield.org/block.txt'
+GREENSNOW='https://blocklist.greensnow.co/greensnow.txt'
+SPAMHAUS_DROP_V4='https://www.spamhaus.org/drop/drop.txt'
+SPAMHAUS_DROP_V6='https://www.spamhaus.org/drop/dropv6.txt'
+STOPFORUMSPAM='https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt'
+
 chown root:_pkgfetch "${TMPFILE}"
 chmod 0660 "${TMPFILE}"
 
 # download IP blocklists and parse
-/usr/bin/su -s/bin/ksh _pkgfetch -c "/usr/bin/ftp -Vo - \
-https://iplists.firehol.org/files/firehol_level1.netset \
-awk '/^[1-9].*[0-9]$/' | cut -d ' ' -f1 | sort -uV | tee ${TMPFILE}" >/dev/null
+/usr/bin/su -s/bin/ksh _pkgfetch -c "/usr/bin/ftp -VMo - \
+        ${BLOCKLISTDE_BOTS} ${BLOCKLISTDE_LONGTERM} ${BLOCKLISTDE_TELEPHONY} \
+        ${GREENSNOW} ${SPAMHAUS_DROP_V4} ${SPAMHAUS_DROP_V6} ${STOPFORUMSPAM} | \
+        awk '/^[1-9]/' | cut -d ' ' -f1 | \
+        sort -uV | tee -a ${TMPFILE}" >/dev/null
+/usr/bin/su -s/bin/ksh _pkgfetch -c "/usr/bin/ftp -VMo - \
+        ${DSHIELD_TOP20} | \
+        awk '/^[1-9]/ {print \$1, \$3}' | \
+        sed 's/\ /\//' | sort -uV | tee -a ${TMPFILE}" >/dev/null
 
 # copy into place
 if [[ -f "${CONF}" ]]; then
