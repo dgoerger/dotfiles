f53d79a57c5682611e607754cb84a9f80d95abef
David Goerger <dgoerger@daemonica.net>
2024-10-18 11:02:52 -0400

    some quality of life improvements for gerrit_upload.sh
    - install the commit-msg hook if missing
    - refuse to amend commits written by someone else
    - offer to claim ownership of a commit if you're super sure
    - detect dirty worktrees, and offer to fix
    - run "pre-commit" as a pre-upload step, i.e. do not force pre-commit to
      pass e.g. fixup or work-in-progress commits before we're ready to
      upload something for peer review
    
    Change-Id: Id123d0455f262f71f223fa3e894b2ea25c51c246
    Reviewed-on: https://review.daemonica.net/c/dgoerger/dotfiles/+/544
    Reviewed-by: David Goerger <dgoerger@daemonica.net>

--- bin/gerrit_uploader.sh
+++ bin/gerrit_uploader.sh
@@ -5,15 +5,138 @@ usage() {
 	printf "usage:\n\tgerrit_uploader.sh [target_branch]\n\n"
 }
 
+if git remote get-url --all gerrit >/dev/null 2>&1; then
+	REMOTE='gerrit'; readonly REMOTE
+elif git remote get-url --all origin >/dev/null 2>&1; then
+	REMOTE='origin'; readonly REMOTE
+else
+	printf 'The current directory either is not a git repo, or has no remote url configured.\n'
+	exit 1
+fi
+
+# query git author information so we can compare with the author of the previous commit
+if [[ -n "${GIT_AUTHOR_EMAIL}" ]]; then
+	author_email="${GIT_AUTHOR_EMAIL}"
+elif git config get user.email >/dev/null 2>&1; then
+	author_email="$(git config get user.email 2>/dev/null)"
+else
+	printf "ERROR: please configure 'git config set user.email user@example.com'\n"
+	exit 1
+fi
+
+does_previous_commit_have_same_author() {
+	# warn if we're amending or uploading someone else's commit
+	if [[ "${author_email}" != "$(git show -s --pretty=%ae 2>/dev/null)" ]]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+if ! git diff --quiet >/dev/null 2>&1; then
+	printf "WARNING: the current worktree is dirty. Would you like to\n"
+	printf "- (c)reate a new commit\n"
+	printf "- (a)mend the previous commit\n"
+	printf "- (p)rint the diff and exit\n\n"
+	printf "Selection: "
+	read -r createOrAmend
+	case "${createOrAmend}" in
+		c|create)
+			git commit -a
+			;;
+		a|amend)
+			if ! does_previous_commit_have_same_author; then
+				printf "ERROR: The previous commit has a different author.\n"
+				printf "       Please create a new commit.\n\n"
+				printf "prev commit: %s\n" "$(git show -s --pretty=%ae 2>/dev/null)"
+				printf "your email: %s\n" "${author_email}"
+				exit 1
+			fi
+			git commit -a --amend
+			;;
+		p|print)
+			git diff
+			exit 1
+			;;
+		*)
+			exit 1
+			;;
+	esac
+elif ! does_previous_commit_have_same_author; then
+	printf "WARNING: You're about to upload a change written by '%s'.\n" "$(git show -s --pretty=%ae 2>/dev/null)"
+	printf "Would you like to\n"
+	printf "- (c)ontinue anyway\n"
+	printf "- (r)eset the author (claim authorship)\n"
+	printf "- (p)rint the diff and exit\n\n"
+	printf "Selection: "
+	read -r continueResetAbort
+	case "${continueResetAbort}" in
+		c|continue)
+			;;
+		r|reset)
+			printf "really claim authorship? (y/N) "
+			read -r yesno
+			case "${yesno}" in
+				y|yes|Y|YES)
+					git commit --amend --reset-author --no-edit
+					;;
+				*)
+					printf "cancelling upload..\n\n"
+					exit 1
+					;;
+			esac
+			;;
+		p|print)
+			git show --pretty=medium
+			exit 1
+			;;
+		*)
+			exit 1
+			;;
+	esac
+fi
+
 # TODO: add support for git-worktree(1)
 if [[ ! -x "$(git rev-parse --absolute-git-dir)/hooks/commit-msg" ]] || ! grep -i gerrit "$(git rev-parse --absolute-git-dir)/hooks/commit-msg" >/dev/null 2>&1; then
 	# IF commit-msg is not executable (or does not exist) OR commit-msg exists but is unrelated to gerrit, THEN
 	printf "This script can only be used to upload changes to Gerrit Code Review.\n"
-	printf "No commit-msg hook found.\n"
-	exit 1
+	printf "No commit-msg hook found.\n\n"
+	remote_clone_url="$(git remote get-url "${REMOTE}" 2>/dev/null)"; readonly remote_clone_url
+	case "${remote_clone_url}" in
+		ssh*)
+			commit_msg_url="$(printf 'https://%s/tools/hooks/commit-msg' "$(echo "${remote_clone_url}" | awk -F':' '{print $2}' | sed 's/.*\@//' | sed 's/\/\///')")"
+			;;
+		https*)
+			commit_msg_url="$(printf 'https://%s/tools/hooks/commit-msg' "$(echo "${remote_clone_url}" | awk -F'/' '{print $3}' | sed 's/.*\@//' | sed 's/\/\///')")"
+			;;
+		*)
+			exit 1
+			;;
+	esac
+	readonly commit_msg_url
+	printf "Install the hook and amend the most recent commit to add a Gerrit Change-Id? (y/N)\n"
+	read -r yesno
+	case "${yesno}" in
+		y*|Y*)
+			mkdir -p "$(git rev-parse --git-dir)/hooks"
+			if [[ -f "$(git rev-parse --git-dir)/hooks/commit-msg" ]]; then
+				timestamp="$(date +%Y%m%d%H%M%S)"; readonly timestamp
+				mv "$(git rev-parse --git-dir)/hooks/commit-msg" "$(git rev-parse --git-dir)/hooks/commit-msg.${timestamp}"
+			fi
+			curl -Lo "$(git rev-parse --git-dir)/hooks/commit-msg" "${commit_msg_url}"
+			chmod +x "$(git rev-parse --git-dir)/hooks/commit-msg"
+			if does_previous_commit_have_same_author; then
+				git commit --amend --no-edit
+			else
+				printf "ERROR: The previous commit has a different author! Refusing to amend.\n"
+				exit 1
+			fi
+			;;
+		*)
+			exit 1
+			;;
+	esac
 fi
-
-# TODO: warn if the worktree contains unstaged commits, and offer to amend them into the current commit if the current author matches the author of the previous commit
 
 if [[ "${#}" == '0' ]]; then
 	UPSTREAM_BRANCH="$(git rev-parse --symbolic-full-name @\{u\} 2>/dev/null | awk -F'/' '{print $NF}')"
@@ -40,7 +163,7 @@ else
 fi
 
 if [[ -z "${UPSTREAM_BRANCH}" ]]; then
-	printf "No upstream branch found. Please configure an upstream branch,\n"
+	printf "No upstream branch found. Please configure an upstream branch\n"
 	printf "using, e.g., 'git branch -u origin/main'\n\n"
 	exit 1
 elif ! git rev-parse --remotes "${UPSTREAM_BRANCH}" >/dev/null 2>&1; then
@@ -57,23 +180,10 @@ if [[ "${UPSTREAM_BRANCH}" != 'main' ]] && [[ "${UPSTREAM_BRANCH}" != 'master' ]
 	fi
 fi
 
-if git remote get-url --all gerrit >/dev/null 2>&1; then
-	REMOTE='gerrit'; readonly REMOTE
-elif git remote get-url --all origin >/dev/null 2>&1; then
-	REMOTE='origin'; readonly REMOTE
-else
-	printf "please configure a remote url\n"
-	exit 1
-fi
-
-# verify that pre-commit, if configured, is enabled
-if [[ -r "$(git rev-parse --show-toplevel)/.pre-commit-config.yaml" ]] && [[ ! -x "$(git rev-parse --absolute-git-dir)/hooks/pre-commit" ]]; then
-	printf "pre-commit not configured. Fixing and running linters.\n"
-	printf "This may take a few minutes...\n"
-	cd "$(git rev-parse --show-toplevel)"
-	pre-commit install
-	cd -
+# run pre-commit as a pre-upload step
+if [[ -r "$(git rev-parse --show-toplevel)/.pre-commit-config.yaml" ]]; then
 	pre-commit run --from-ref "$(git merge-base "@{u}" HEAD)" --to-ref HEAD
 fi
-# TODO: support uploading a rev other than HEAD; conditional on rewrite in a non-bourne language
+
+# TODO: support uploading a rev other than HEAD
 exec git push "${REMOTE}" "HEAD:refs/for/${UPSTREAM_BRANCH}"
